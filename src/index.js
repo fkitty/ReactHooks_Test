06545773101b1f9e32b49a5725645b6838bb5a0a
脱安琪 <453001363@qq.com>
2020-07-13 22:54:34 +0800

    feat: useState不能写在if中

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let _state = [];
+let index = 0;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function myUseState(initialValue) {
+  const currentIndex = index;
+  index += 1;
+  _state[currentIndex] = _state[currentIndex] || initialValue;
+  const setState = newState => {
+    _state[currentIndex] = newState;
+    render();
+  };
+  return [_state[currentIndex], setState];
+}
+
+// 教学需要，不用在意 render 的实现
+const render = () => {
+  index = 0;
+  ReactDOM.render(<App />, rootElement);
+};
+
+function App() {
+  const [n, setN] = React.useState(0);
+  let m, setM;
+  // if (n % 2 === 1) {
+  //   [m, setM] = React.useState(0);
+  // }
+  [m, setM] = React.useState(0);
+  return (
+    <div className="App">
+      <p>{n}</p>
+      <p>
+        <button onClick={() => setN(n + 1)}>+1</button>
+      </p>
+      <p>{m}</p>
+      <p>
+        <button onClick={() => setM(m + 1)}>+1</button>
+      </p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, rootElement);
