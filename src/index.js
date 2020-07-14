2df967803d2b2625d09e2814beb9eb0b25d431e4
脱安琪 <453001363@qq.com>
2020-07-14 23:49:53 +0800

    feat: 先点击加1再log正确，先log再加log出了旧数据

--- src/index.js
+++ src/index.js
@@ -22,22 +22,37 @@ const render = () => {
   ReactDOM.render(<App />, rootElement);
 };
 
+// function App() {
+//   const [n, setN] = React.useState(0);
+//   let m, setM;
+//   // if (n % 2 === 1) {
+//   //   [m, setM] = React.useState(0);
+//   // }
+//   [m, setM] = React.useState(0);
+//   return (
+//     <div className="App">
+//       <p>{n}</p>
+//       <p>
+//         <button onClick={() => setN(n + 1)}>+1</button>
+//       </p>
+//       <p>{m}</p>
+//       <p>
+//         <button onClick={() => setM(m + 1)}>+1</button>
+//       </p>
+//     </div>
+//   );
+// }
+
+
 function App() {
   const [n, setN] = React.useState(0);
-  let m, setM;
-  // if (n % 2 === 1) {
-  //   [m, setM] = React.useState(0);
-  // }
-  [m, setM] = React.useState(0);
+  const log = () => setTimeout(() => console.log(`n: ${n}`), 3000);
   return (
     <div className="App">
       <p>{n}</p>
       <p>
         <button onClick={() => setN(n + 1)}>+1</button>
-      </p>
-      <p>{m}</p>
-      <p>
-        <button onClick={() => setM(m + 1)}>+1</button>
+        <button onClick={log}>log</button>
       </p>
     </div>
   );
