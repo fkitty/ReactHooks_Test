a01b1082558a391fcc9a4b136901de4f0f0043c9
脱安琪 <453001363@qq.com>
2020-07-16 00:11:56 +0800

    feat: 使用useContext实现切换主题的例子（在于全局变量）

--- src/index.js
+++ src/index.js
@@ -74,25 +74,74 @@ const rootElement = document.getElementById("root");
  * 只有一个n,current值，不会随着变化而更新
  * 手动触发更新（但是不是react的想要的思想，适合vue3）
  */
+// function App() {
+//   const nRef = React.useRef(0); // ref就是一个对象 {current: 0}  
+//   const log = () => setTimeout(() => console.log(`n: ${nRef.current}`), 1000);
+//   // const [n, setN] = React.useState(null); // setN的值和n不相同，APP就会更新
+//   // const setN = React.useState(null)[1]; // 上面一行代码的简化
+//   const update = React.useState(null)[1]; // 不关心是不是n, 所以改变变量名称
+//   return (
+//     <div className="App">
+//       {/* 因为在执行‘nRef.current += 1’的时候，不会让App重新渲染 */}
+//       <p>{nRef.current} 这里并不能实时更新</p>
+//       <p>
+//         <button onClick={() => {
+//           nRef.current += 1;
+//           update(nRef.current) // 使得App重新渲染
+//         }}>+1</button>
+//         <button onClick={log}>log</button>
+//       </p>
+//     </div>
+//   );
+// }
+
+
+
+/**
+ * useContext切换主题的例子（全局变量）
+ */
+const themeContext = React.createContext(null); // 初始化一个上下文
+
 function App() {
-  const nRef = React.useRef(0); // ref就是一个对象 {current: 0}  
-  const log = () => setTimeout(() => console.log(`n: ${nRef.current}`), 1000);
-  // const [n, setN] = React.useState(null); // setN的值和n不相同，APP就会更新
-  // const setN = React.useState(null)[1]; // 上面一行代码的简化
-  const update = React.useState(null)[1]; // 不关心是不是n, 所以改变变量名称
+  // 全局切换主题
+  const [theme, setTheme] = React.useState("red");
   return (
-    <div className="App">
-      {/* 因为在执行‘nRef.current += 1’的时候，不会让App重新渲染 */}
-      <p>{nRef.current} 这里并不能实时更新</p>
-      <p>
-        <button onClick={() => {
-          nRef.current += 1;
-          update(nRef.current) // 使得App重新渲染
-        }}>+1</button>
-        <button onClick={log}>log</button>
-      </p>
+    // value的意思就是：对全局变量进行一次赋值，它的值是一个对象
+    // value={ js的开始{ theme: theme的缩写, setTheme }}
+    // 一开始就把全局变量的初始值赋值为一个对象，这个对象有theme和setTheme两个属性
+    // themeContext.Provider这个上下文全局变量 ：它的作用域就从themeContext.Provider这里开始到结束，只有在这里可以用theme和setTheme
+    <themeContext.Provider value={{ theme, setTheme }}>
+      <div className={`App ${theme}`}>
+        <p>{theme}</p>
+        <div>
+          <ChildA />
+        </div>
+        <div>
+          <ChildB />
+        </div>
+      </div>
+    </themeContext.Provider>
+  );
+}
+
+function ChildA() {
+  // 从全局变量themeContext中读取setTheme
+  const { setTheme } = React.useContext(themeContext);
+  return (
+    <div>
+      <button onClick={() => setTheme("red")}>red</button>
     </div>
   );
 }
+
+function ChildB() {
+  const { setTheme } = React.useContext(themeContext);
+  return (
+    <div>
+      <button onClick={() => setTheme("blue")}>blue</button>
+    </div>
+  );
+}
+
 
 ReactDOM.render(<App />, rootElement);
